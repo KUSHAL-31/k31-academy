9e0f624f400ecebc9da50aa49e2315002a987e39
KUSHAL-31 <s.kushal31@gmail.com>
2023-12-26 10:58:22 +0530

    user model completed

--- server/app.ts
+++ server/app.ts
@@ -1,31 +1,36 @@
-require('dotenv').config();
-import express, { NextFunction, Request, Response } from 'express';
+require("dotenv").config();
+import express, { NextFunction, Request, Response } from "express";
 export const app = express();
-import cors from 'cors';
-import cookieParser from 'cookie-parser'
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { ErrorMiddleware } from "./middleware/error";
 
 // body parser
-app.use(express.json({ limit: "50mb" }))
+app.use(express.json({ limit: "50mb" }));
 
 //cookie parser
 app.use(cookieParser());
 
 // cors => cross origin resource sharing
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.ORIGIN,
-}));
+  })
+);
 
 // Testing API
-app.get('/dev/test', (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).json({
-        success: true,
-        message: "Test API working successfully",
-    });
+app.get("/dev/test", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json({
+    success: true,
+    message: "Test API working successfully",
+  });
 });
 
 // Unknown routes
 app.all("*", (req: Request, res: Response, next: NextFunction) => {
-    const err = new Error(`Route : ${req.originalUrl} not found`) as any;
-    err.statusCode = 404;
-    next(err);
+  const err = new Error(`Route : ${req.originalUrl} not found`) as any;
+  err.statusCode = 404;
+  next(err);
 });
+
+app.use(ErrorMiddleware);
---
+++ server/models/user.model.ts
@@ -0,0 +1,85 @@
+import mongoose, { Document, Model, Schema } from "mongoose";
+import bcrypt from "bcryptjs";
+
+const emailRegexPattern: RegExp = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/;
+
+export interface User extends Document {
+  name: string;
+  email: string;
+  type: string;
+  password: string;
+  avatar: {
+    public_id: string;
+    url: string;
+  };
+  role: string;
+  isVerified: boolean;
+  courses: Array<{ courseId: string }>;
+  comparePassword: (password: string) => Promise<boolean>;
+}
+
+const userSchema: Schema<User> = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter your email"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please enter your email"],
+      validate: {
+        validator: function (value: string) {
+          return emailRegexPattern.test(value);
+        },
+        message: "Please enter your email",
+      },
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please enter your email"],
+      minlength: [6, "Password must be atleast of 6 characters"],
+      select: false,
+    },
+    avatar: {
+      public_id: String,
+      url: String,
+    },
+    role: {
+      type: String,
+      default: "user",
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    courses: [
+      {
+        courseId: String,
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+// Hash password before storing
+
+userSchema.pre<User>("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// compare password
+
+userSchema.methods.comparePassword = async function (
+  enteredPassword: string
+): Promise<boolean> {
+  return bcrypt.compare(enteredPassword, this.password);
+};
+
+const userModel: Model<User> = mongoose.model("User", userSchema);
+
+export default userModel;
